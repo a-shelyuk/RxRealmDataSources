12e4c83187b0db136789a4638a629c3f8db96a98
Artem <artem.shelyuk@ourbond.com>
2024-04-04 19:17:43 -0400

    Add section header support for RxCollectionViewRealmDataSource

--- Pod/Classes/RxCollectionViewRealmDataSource.swift
+++ Pod/Classes/RxCollectionViewRealmDataSource.swift
@@ -11,66 +11,89 @@ import RealmSwift
 import RxSwift
 import RxCocoa
 import RxRealm
+import UIKit
 
-#if os(iOS)
-    // MARK: - iOS / UIKit
+public typealias CollectionCellFactory<E: Object> = (RxCollectionViewRealmDataSource<E>, UICollectionView, IndexPath, E) -> UICollectionViewCell
+public typealias CollectionCellConfig<E: Object, CellType: UICollectionViewCell> = (CellType, IndexPath, E) -> Void
 
-    import UIKit
+open class RxCollectionViewRealmDataSource <E: Object>: NSObject, UICollectionViewDataSource {
+    private var items: AnyRealmCollection<E>?
 
-    public typealias CollectionCellFactory<E: Object> = (RxCollectionViewRealmDataSource<E>, UICollectionView, IndexPath, E) -> UICollectionViewCell
-    public typealias CollectionCellConfig<E: Object, CellType: UICollectionViewCell> = (CellType, IndexPath, E) -> Void
+    // MARK: - Configuration
 
-    open class RxCollectionViewRealmDataSource <E: Object>: NSObject, UICollectionViewDataSource {
-        private var items: AnyRealmCollection<E>?
+    public var collectionView: UICollectionView?
+    public var animated = true
 
-        // MARK: - Configuration
+    // MARK: - Init
+    public let cellIdentifier: String
+    public let cellFactory: CollectionCellFactory<E>
 
-        public var collectionView: UICollectionView?
-        public var animated = true
+    public init(cellIdentifier: String, cellFactory: @escaping CollectionCellFactory<E>) {
+        self.cellIdentifier = cellIdentifier
+        self.cellFactory = cellFactory
+    }
 
-        // MARK: - Init
-        public let cellIdentifier: String
-        public let cellFactory: CollectionCellFactory<E>
-
-        public init(cellIdentifier: String, cellFactory: @escaping CollectionCellFactory<E>) {
-            self.cellIdentifier = cellIdentifier
-            self.cellFactory = cellFactory
+    public init<CellType>(cellIdentifier: String, cellType: CellType.Type, cellConfig: @escaping CollectionCellConfig<E, CellType>) where CellType: UICollectionViewCell {
+        self.cellIdentifier = cellIdentifier
+        self.cellFactory = {ds, cv, ip, model in
+            let cell = cv.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: ip) as! CellType
+            cellConfig(cell, ip, model)
+            return cell
         }
+    }
 
-        public init<CellType>(cellIdentifier: String, cellType: CellType.Type, cellConfig: @escaping CollectionCellConfig<E, CellType>) where CellType: UICollectionViewCell {
-            self.cellIdentifier = cellIdentifier
-            self.cellFactory = {ds, cv, ip, model in
-                let cell = cv.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: ip) as! CellType
-                cellConfig(cell, ip, model)
-                return cell
-            }
+    public init<CellType>(cellClass: CellType.Type, cellConfig: @escaping CollectionCellConfig<E, CellType>) where CellType: UICollectionViewCell, CellType: ReusableView {
+        self.cellIdentifier = cellClass.reuseIdentifier
+        self.cellFactory = { ds, cv, ip, model in
+            let cell: CellType = cv.dequeueReusableCell(forIndexPath: ip)
+            cellConfig(cell, ip, model)
+
+            return cell
         }
+    }
 
-        // MARK: - Data access
-        public func model(at indexPath: IndexPath) -> E {
-            return items![indexPath.row]
+    var headerViewClass: AnyClass? {
+        didSet { collectionView?.register(headerViewClass, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "SectionHeader") }
+    }
+
+    var headerViewNib: UINib? {
+        didSet { collectionView?.register(headerViewNib, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "SectionHeader") }
+    }
+
+    // MARK: - Data access
+    public func model(at indexPath: IndexPath) -> E {
+        return items![indexPath.row]
+    }
+
+    // MARK: - UICollectionViewDataSource protocol
+    public func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+
+    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items?.count ?? 0
+    }
+
+    public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return cellFactory(self, collectionView, indexPath, items![indexPath.row])
+    }
+
+    public func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        if kind == UICollectionView.elementKindSectionHeader {
+            let view: UICollectionReusableView = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "SectionHeader", for: indexPath)
+            return view
+        } else {
+            fatalError()
         }
+    }
 
-        // MARK: - UICollectionViewDataSource protocol
-        public func numberOfSections(in collectionView: UICollectionView) -> Int {
-            return 1
+    // MARK: - Applying changeset to the collection view
+    private let fromRow = {(row: Int) in return IndexPath(row: row, section: 0)}
+
+    func applyChanges(items: AnyRealmCollection<E>, changes: RealmChangeset?) {
+        if self.items == nil {
+            self.items = items
         }
-
-        public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-            return items?.count ?? 0
-        }
-
-        public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-            return cellFactory(self, collectionView, indexPath, items![indexPath.row])
-        }
-
-        // MARK: - Applying changeset to the collection view
-        private let fromRow = {(row: Int) in return IndexPath(row: row, section: 0)}
-
-        func applyChanges(items: AnyRealmCollection<E>, changes: RealmChangeset?) {
-            if self.items == nil {
-                self.items = items
-            }
 
         guard let collectionView = collectionView else {
             fatalError("You have to bind a collection view to the data source.")
@@ -99,112 +122,3 @@ import RxRealm
         }, completion: nil)
     }
 }
-
-#elseif os(OSX)
-// MARK: - macOS / Cocoa
-
-import Cocoa
-
-    public typealias CollectionItemFactory<E: Object> = (RxCollectionViewRealmDataSource<E>, NSCollectionView, IndexPath, E) -> NSCollectionViewItem
-    public typealias CollectionItemConfig<E: Object, ItemType: NSCollectionViewItem> = (ItemType, IndexPath, E) -> Void
-
-    open class RxCollectionViewRealmDataSource <E: Object>: NSObject, NSCollectionViewDataSource {
-
-        private var items: AnyRealmCollection<E>?
-
-        // MARK: - Configuration
-
-        public var collectionView: NSCollectionView?
-        public var animated = true
-
-        // MARK: - Init
-        public let itemIdentifier: String
-        public let itemFactory: CollectionItemFactory<E>
-
-        public weak var delegate: NSCollectionViewDelegate?
-        public weak var dataSource: NSCollectionViewDataSource?
-
-        public init(itemIdentifier: String, itemFactory: @escaping CollectionItemFactory<E>) {
-            self.itemIdentifier = itemIdentifier
-            self.itemFactory = itemFactory
-        }
-
-        public init<ItemType>(itemIdentifier: String, itemType: ItemType.Type, itemConfig: @escaping CollectionItemConfig<E, ItemType>) where ItemType: NSCollectionViewItem {
-            self.itemIdentifier = itemIdentifier
-            self.itemFactory = { ds, cv, ip, model in
-              let item = cv.makeItem(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: itemIdentifier), for: ip) as! ItemType
-                itemConfig(item, ip, model)
-                return item
-            }
-        }
-
-        // MARK: - NSCollectionViewDataSource protocol
-        public func numberOfSections(in collectionView: NSCollectionView) -> Int {
-            return 1
-        }
-
-        public func collectionView(_ collectionView: NSCollectionView, numberOfItemsInSection section: Int) -> Int {
-            return items?.count ?? 0
-        }
-
-        @available(OSX 10.11, *)
-        public func collectionView(_ collectionView: NSCollectionView, itemForRepresentedObjectAt indexPath: IndexPath) -> NSCollectionViewItem {
-            return itemFactory(self, collectionView, indexPath, items![indexPath.item])
-        }
-
-        // MARK: - Proxy unimplemented data source and delegate methods
-        open override func responds(to aSelector: Selector!) -> Bool {
-            if RxCollectionViewRealmDataSource.instancesRespond(to: aSelector) {
-                return true
-            } else if let delegate = delegate {
-                return delegate.responds(to: aSelector)
-            } else if let dataSource = dataSource {
-                return dataSource.responds(to: aSelector)
-            } else {
-                return false
-            }
-        }
-
-        open override func forwardingTarget(for aSelector: Selector!) -> Any? {
-            return delegate ?? dataSource
-        }
-
-        // MARK: - Applying changeset to the collection view
-        private let fromRow = {(row: Int) in return IndexPath(item: row, section: 0)}
-
-        func applyChanges(items: AnyRealmCollection<E>, changes: RealmChangeset?) {
-            if self.items == nil {
-                self.items = items
-            }
-
-            guard let collectionView = collectionView else {
-                fatalError("You have to bind a collection view to the data source.")
-            }
-
-            guard animated else {
-                collectionView.reloadData()
-                return
-            }
-
-            guard let changes = changes else {
-                collectionView.reloadData()
-                return
-            }
-
-            let lastItemCount = collectionView.numberOfItems(inSection: 0)
-            guard items.count == lastItemCount + changes.inserted.count - changes.deleted.count else {
-                collectionView.reloadData()
-                return
-            }
-
-            collectionView.performBatchUpdates({[unowned self] in
-                //TODO: this should be animated, but doesn't seem to be?
-                collectionView.animator().deleteItems(at: Set(changes.deleted.map(self.fromRow)))
-                collectionView.animator().reloadItems(at: Set(changes.updated.map(self.fromRow)))
-                collectionView.animator().insertItems(at: Set(changes.inserted.map(self.fromRow)))
-            }, completionHandler: nil)
-        }
-    }
-
-
-#endif
